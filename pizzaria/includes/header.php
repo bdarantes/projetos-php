d5fae33005751c0297c83382f37b486c5f5392b7
BrazD <brazdanarantes@gmail.com>
2025-12-30 10:39:41 -0300

    projeto03-pizzaria/criação do header.php

---
+++ pizzaria/includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>Pizzaria O Arantão</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="description" content="Pizzaria artesanal com massas frescas, ingredientes selecionados e forno a lenha.">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<header class="header">
+    <div class="container">
+        <h1 class="logo">Pizzaria O Arantão</h1>
+
+        <nav class="menu">
+            <ul>
+                <li><a href="index.php">Início</a></li>
+                <li><a href="contato.php">Contato</a> </li>
+            </ul>
+        </nav>
+    </div>
+</header>
